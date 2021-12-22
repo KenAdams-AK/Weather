4768fbcdf733f8700b2443ab648de6f44274c207
KenAdams777 <andrey.kalyshenko@gmail.com>
2021-12-22 15:59:33 +0200

    Updates

---
+++ precache-manifest.94e95409246375531ef16c57afb60459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b67d5525c9339e79089ab14d52c490",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "cf9eb4ee2dfa2d8e6846",
+    "url": "/Weather/static/css/main.ee3045cb.chunk.css"
+  },
+  {
+    "revision": "02f3b7af59cdba20035b",
+    "url": "/Weather/static/js/2.62b4258c.chunk.js"
+  },
+  {
+    "revision": "7b6e4ccea9ef86d76a94e5ca35b1e724",
+    "url": "/Weather/static/js/2.62b4258c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9eb4ee2dfa2d8e6846",
+    "url": "/Weather/static/js/main.43a639bf.chunk.js"
+  },
+  {
+    "revision": "444565677c5457d4f355",
+    "url": "/Weather/static/js/runtime-main.bb8f6e8d.js"
+  }
+]);
